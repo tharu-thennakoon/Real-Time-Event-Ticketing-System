37ff2e4103ec6bb688b1c8c278803bc08677b4c3
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-12-11 02:15:38 +0530

    clear previous project

--- frontend/ticketing-system-frontend/src/App.js
+++ frontend/ticketing-system-frontend/src/App.js
@@ -1,29 +1,19 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import ConfigurationPage from './pages/ConfigurationPage';
-import TicketPage from './pages/TicketPage';
-import ControlPage from './pages/ControlPage';
-import LogPage from './pages/LogPage';
-import './App.css';
+import AnalyticsPage from './pages/AnalyticsPage';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/configuration" element={<ConfigurationPage />} />
-        <Route path="/ticket-status" element={<TicketPage />} />
-        <Route path="/control-panel" element={<ControlPage />} />
-        <Route path="/logs" element={<LogPage />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/analytics" element={<AnalyticsPage />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
